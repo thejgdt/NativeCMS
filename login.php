2c87ddc634740402122256d0633cda18bf5ebf32
thejgdt <theaberchio@gmail.com>
2024-04-22 15:34:20 +0700

    feat: add login form

--- login.php
+++ login.php
@@ -30,3 +30,40 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $error = "Username atau password salah";
     }
 }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+
+<body>
+    <div>
+        <h2>Login</h2>
+
+        <!-- Form Login -->
+        <form method="post">
+            <div>
+                <label for="username">Username</label>
+                <input type="text" name="username" id="username">
+            </div>
+            <div>
+                <label for="password">Password</label>
+                <input type="password" name="password" id="password">
+            </div>
+            <div>
+                <input type="submit" value="Login">
+            </div>
+        </form>
+
+        <?php
+        if (isset($error)) echo "<p>$error</p>";
+        ?>
+    </div>
+</body>
+
+</html>
