963095d191d1c119ccdc5d4589691abcac01a7e5
thejgdt <theaberchio@gmail.com>
2024-04-22 16:13:29 +0700

    feat: add show posts function to the sidebar menu

--- sidebar.php
+++ sidebar.php
@@ -2,6 +2,7 @@
     <h2 class="text-xl font-semibold mb-4">Menu</h2>
     <ul>
         <li><a href="form_submit_post.php" class="text-gray-700 hover:text-gray-900">Buat Postingan</a></li>
+        <li><a href="show_post.php" class="text-gray-700 hover:text-gray-900">Daftar Postingan</a></li>
         <li><a href="form_edit_post.php" class="text-gray-700 hover:text-gray-900">Edit Postingan</a></li>
         <li><a href="form_hapus_post.php" class="text-gray-700 hover:text-gray-900">Hapus Postingan</a></li>
         <!-- Tambahkan lebih banyak menu sesuai kebutuhan -->
