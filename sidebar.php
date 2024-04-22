7db234dd3f27d49569753473058467987be53186
thejgdt <theaberchio@gmail.com>
2024-04-22 15:47:14 +0700

    implement sidebar templates

---
+++ sidebar.php
@@ -0,0 +1,9 @@
+<aside class="bg-gray-200 w-1/4 min-h-screen p-4">
+    <h2 class="text-xl font-semibold mb-4">Menu</h2>
+    <ul>
+        <li><a href="form_submit_post.php" class="text-gray-700 hover:text-gray-900">Buat Postingan</a></li>
+        <li><a href="form_edit_post.php" class="text-gray-700 hover:text-gray-900">Edit Postingan</a></li>
+        <li><a href="form_hapus_post.php" class="text-gray-700 hover:text-gray-900">Hapus Postingan</a></li>
+        <!-- Tambahkan lebih banyak menu sesuai kebutuhan -->
+    </ul>
+</aside>
