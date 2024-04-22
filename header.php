4838e4fa4e4ff810c99d6cf19379fde26dde3df3
thejgdt <theaberchio@gmail.com>
2024-04-22 21:40:12 +0700

    refactor: code cleanup the <head> element has been called via index.php

--- header.php
+++ header.php
@@ -1,14 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>CMS Saya</title>
-    <!-- Tambahkan Tailwind CSS CDN -->
-    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
-</head>
-
 <body>
     <header class="bg-indigo-600 py-4">
         <div class="container mx-auto px-4 flex justify-between items-center">
