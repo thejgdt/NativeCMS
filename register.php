ad1755401897487c6569f64e17a1dd80c5c42bc0
thejgdt <theaberchio@gmail.com>
2024-04-22 15:41:55 +0700

    feat: add register form

--- register.php
+++ register.php
@@ -17,3 +17,36 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         echo "Error: " . $sql . "<br>" . $conn->error;
     }
 }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Register</title>
+</head>
+
+<body>
+    <div>
+        <h2>Register</h2>
+
+        <!-- Form Pendaftaran -->
+        <form method="post">
+            <div>
+                <label for="username">Username</label>
+                <input type="text" name="username" id="username">
+            </div>
+            <div>
+                <label for="password">Password</label>
+                <input type="password" name="password" id="password">
+            </div>
+            <div>
+                <input type="submit" value="Register">
+            </div>
+        </form>
+    </div>
+</body>
+
+</html>
