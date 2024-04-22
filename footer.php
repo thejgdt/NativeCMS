7f4b670d032897e3af0a9b9c0fe87ededcb87631
thejgdt <theaberchio@gmail.com>
2024-04-22 15:47:41 +0700

    implement footer templates

---
+++ footer.php
@@ -0,0 +1,8 @@
+<footer class="bg-gray-200 py-4">
+    <div class="container mx-auto px-4">
+        <p class="text-center text-gray-600">Hak Cipta &copy; <?php echo date("Y"); ?> CMS Saya</p>
+    </div>
+</footer>
+</body>
+
+</html>
