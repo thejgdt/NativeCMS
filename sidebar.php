f8f7cea2c4700e2af6c1dbd0aa148c8749bc5b89
thejgdt <theaberchio@gmail.com>
2024-04-22 21:33:48 +0700

    feat: trigger function throughout the box

--- sidebar.php
+++ sidebar.php
@@ -1,8 +1,20 @@
 <aside class="bg-gray-200 w-1/4 min-h-screen p-4">
     <h2 class="text-xl font-semibold mb-4">Menu</h2>
     <ul>
-        <li><a href="form_submit_post.php" class="text-gray-700 hover:text-gray-900">Buat Postingan</a></li>
-        <li><a href="show_post.php" class="text-gray-700 hover:text-gray-900">Daftar Postingan</a></li>
+        <li>
+            <a href="form_submit_post.php">
+                <div class="bg-white rounded-lg p-2 mb-2 transition duration-300 ease-in-out hover:bg-gray-100 focus:bg-gray-100">
+                    <p class="text-gray-700 hover:text-gray-900 focus:text-gray-900">Buat Postingan</p>
+                </div>
+            </a>
+        </li>
+        <li>
+            <a href="show_post.php">
+                <div class="bg-white rounded-lg p-2 mb-2 transition duration-300 ease-in-out hover:bg-gray-100 focus:bg-gray-100">
+                    <p class="text-gray-700 hover:text-gray-900 focus:text-gray-900">Daftar Postingan</p>
+                </div>
+            </a>
+        </li>
         <!-- Tambahkan lebih banyak menu sesuai kebutuhan -->
     </ul>
 </aside>
