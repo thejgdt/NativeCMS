25cb3d78de8851b356de754764ed25a2227dafe8
thejgdt <theaberchio@gmail.com>
2024-04-22 16:06:43 +0700

    feat: fix: add functions for the post submission process

---
+++ proses_submit_post.php
@@ -0,0 +1,20 @@
+<?php
+// Sambungkan ke database
+include('koneksi.php');
+
+// Tangkap data dari form
+$title = $_POST['title'];
+$content = $_POST['content'];
+
+// Query untuk memasukkan data ke dalam database
+$sql = "INSERT INTO posts (title, content) VALUES ('$title', '$content')";
+if ($conn->query($sql) === TRUE) {
+    // Jika berhasil ditambahkan, arahkan kembali ke halaman utama
+    header("Location: index.php");
+    exit();
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+// Tutup koneksi database
+$conn->close();
